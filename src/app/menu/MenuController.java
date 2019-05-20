d15609de635b789050ca3ea636727fe62699d64c
Martin Vogel <stift.mobile@gmail.com>
2019-05-20 17:00:57 +0200

    Menu apply explicit mvc

--- src/app/menu/MenuController.java
+++ src/app/menu/MenuController.java
@@ -1,16 +1,6 @@
 package app.menu;
-	
-import java.net.URL;
-import java.util.ResourceBundle;
 
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import app.Util;
 import app.Zustand;
 import app.controlling.ControllingController;
 import app.doc.DocController;
@@ -28,98 +18,14 @@ import app.waren.WarenController;
  * @version 1.0
  * @since   1.0
  */
-public class MenuController implements Initializable, Anmelden {
+public class MenuController implements Anmelden {
 			
-	@FXML
-	private MenuBar nav_bar;
-	@FXML
-	private MenuItem nav_saeule_1;
-	@FXML
-	private MenuItem nav_saeule_2;
-	@FXML
-	private MenuItem nav_saeule_3;
-	@FXML
-	private MenuItem nav_journal;
-	@FXML
-	private MenuItem nav_controlling_ausgaben;
-	@FXML
-	private MenuItem nav_controlling_einnahmen;
-	@FXML
-	private MenuItem nav_controlling_betriebsergebnis;
-	@FXML
-	private MenuItem nav_waren_bestand;
-	@FXML
-	private MenuItem nav_waren_bestellungen;
-	@FXML
-	private MenuItem nav_kraftstoff_bestand;
-	@FXML
-	private MenuItem nav_kraftstoff_bestellungen;
-	@FXML
-	private MenuItem nav_kraftstoff_tanks;
-	@FXML
-	private MenuItem nav_personal_benutzer;
-	@FXML
-	private MenuItem nav_personal_verwaltung;
-	@FXML
-	private MenuItem nav_personal_abmelden;
-	@FXML
-	private MenuItem nav_fenster_dark;
-	@FXML
-	private MenuItem nav_fenster_light;
-	@FXML
-	private MenuItem nav_fenster_doc;
-	
-	@FXML
-	private Button nav_user;
+	private MenuView view = new MenuView(this);
 
-	
-	public MenuController() {
-		new Util().onLoadTop(super.getClass().getResource("Menu.fxml"),this);
-	}
-	
-	@Override
-	public void initialize(URL arg, ResourceBundle res) {
-		nav_bar.setDisable(true);
-		/**/
-		nav_saeule_1.setOnAction(this::onClickNavMenuItem);
-		nav_saeule_2.setOnAction(this::onClickNavMenuItem);
-		nav_saeule_3.setOnAction(this::onClickNavMenuItem);
-        nav_journal.setOnAction(this::onClickNavMenuItem);
-    	nav_controlling_ausgaben.setOnAction(this::onClickNavMenuItem);
-    	nav_controlling_einnahmen.setOnAction(this::onClickNavMenuItem);
-    	nav_controlling_betriebsergebnis.setOnAction(this::onClickNavMenuItem);
-        nav_waren_bestand.setOnAction(this::onClickNavMenuItem);
-        nav_waren_bestellungen.setOnAction(this::onClickNavMenuItem);
-        nav_kraftstoff_bestand.setOnAction(this::onClickNavMenuItem);
-        nav_kraftstoff_bestellungen.setOnAction(this::onClickNavMenuItem);
-        nav_kraftstoff_tanks.setOnAction(this::onClickNavMenuItem);
-        nav_personal_benutzer.setOnAction(this::onClickNavMenuItem); 
-        nav_personal_verwaltung.setOnAction(this::onClickNavMenuItem); 
-        nav_personal_abmelden.setOnAction(this::onClickNavMenuItem);
-        nav_fenster_dark.setOnAction(this::onClickNavMenuItem);
-        nav_fenster_light.setOnAction(this::onClickNavMenuItem);
-        /**/
-        nav_fenster_doc.setOnAction(this::onClickNavMenuItem);
-        /**/
-        nav_user.setOnAction(this::onClickNavButton);
-	}
-
-
-	private void onClickNavButton(ActionEvent event) {
+	void onCall(String id) {
 		if(null == Zustand.getInstance().getBenutzer()) {
 			this.onAbmelden();
 		}else {
-			String id = ((Button)event.getSource()).getId();
-			if(id.equals("nav_user")){this.onAbmelden();}
-			else {System.out.println("no id found "+id);}
-		}
-	}
-	
-	private void onClickNavMenuItem(ActionEvent event) {
-		if(null == Zustand.getInstance().getBenutzer()) {
-			this.onAbmelden();
-		}else {
-			String id = ((MenuItem)event.getSource()).getId();
 			if(id.equals("nav_saeule_1")){new VerkaufController(VerkaufController.SAEULE1);}
 			else if(id.equals("nav_saeule_2")){new VerkaufController(VerkaufController.SAEULE2);}
 			else if(id.equals("nav_saeule_3")){new VerkaufController(VerkaufController.SAEULE3);}
@@ -146,23 +52,21 @@ public class MenuController implements Initializable, Anmelden {
 				ObservableList<String> sheets = zustand.getScene().getStylesheets();
 				sheets.add(zustand.getDesign());
 				sheets.remove(Zustand.DARK);
-			}else if(id.equals("nav_fenster_doc")){new DocController();}
+			}else if(id.equals("nav_fenster_doc")){new DocController();
+			}else if(id.equals("nav_user")){this.onAbmelden();}
 			else {System.out.println("no id found "+id);}
 		}
-		
 	}
 
 	@Override
 	public void onAnmelden() {
 		PersonalRecord benutzer = Zustand.getInstance().getBenutzer();
-		this.nav_user.setText(benutzer.getVorname()+" "+benutzer.getNachname());
-		this.nav_bar.setDisable(false);
+		view.showUser(benutzer.getVorname(),benutzer.getNachname());
 	}
 
 	@Override
 	public void onAbmelden() {
-		this.nav_user.setText("");
-		this.nav_bar.setDisable(true);
+		view.clearUser();
 		new AnmeldenController(this);
 	}
 }
---
+++ src/app/menu/MenuView.java
@@ -0,0 +1,103 @@
+package app.menu;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import app.Util;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+public class MenuView implements Initializable {
+
+	@FXML
+	private MenuBar nav_bar;
+	@FXML
+	private MenuItem nav_saeule_1;
+	@FXML
+	private MenuItem nav_saeule_2;
+	@FXML
+	private MenuItem nav_saeule_3;
+	@FXML
+	private MenuItem nav_journal;
+	@FXML
+	private MenuItem nav_controlling_ausgaben;
+	@FXML
+	private MenuItem nav_controlling_einnahmen;
+	@FXML
+	private MenuItem nav_controlling_betriebsergebnis;
+	@FXML
+	private MenuItem nav_waren_bestand;
+	@FXML
+	private MenuItem nav_waren_bestellungen;
+	@FXML
+	private MenuItem nav_kraftstoff_bestand;
+	@FXML
+	private MenuItem nav_kraftstoff_bestellungen;
+	@FXML
+	private MenuItem nav_kraftstoff_tanks;
+	@FXML
+	private MenuItem nav_personal_benutzer;
+	@FXML
+	private MenuItem nav_personal_verwaltung;
+	@FXML
+	private MenuItem nav_personal_abmelden;
+	@FXML
+	private MenuItem nav_fenster_dark;
+	@FXML
+	private MenuItem nav_fenster_light;
+	@FXML
+	private MenuItem nav_fenster_doc;
+	@FXML
+	private Button nav_user;
+	
+	private MenuController controller;
+	
+	public MenuView(MenuController controller) {
+		this.controller = controller;
+		new Util().onLoadTop(super.getClass().getResource("Menu.fxml"),this);
+	}
+	
+	@Override
+	public void initialize(URL arg, ResourceBundle res) {
+		nav_bar.setDisable(true);
+		/**/
+		nav_saeule_1.setOnAction(event -> {controller.onCall(nav_saeule_1.getId());});
+		nav_saeule_2.setOnAction(event -> {controller.onCall(nav_saeule_2.getId());});
+		nav_saeule_3.setOnAction(event -> {controller.onCall(nav_saeule_3.getId());});
+		nav_journal.setOnAction(event -> {controller.onCall(nav_journal.getId());});
+		/**/
+		nav_controlling_ausgaben.setOnAction(event -> {controller.onCall(nav_controlling_ausgaben.getId());});
+		nav_controlling_einnahmen.setOnAction(event -> {controller.onCall(nav_controlling_einnahmen.getId());});
+		nav_controlling_betriebsergebnis.setOnAction(event -> {controller.onCall(nav_controlling_betriebsergebnis.getId());});
+		/**/
+		nav_waren_bestand.setOnAction(event -> {controller.onCall(nav_waren_bestand.getId());});
+		nav_waren_bestellungen.setOnAction(event -> {controller.onCall(nav_waren_bestellungen.getId());});
+		/**/
+		nav_kraftstoff_bestand.setOnAction(event -> {controller.onCall(nav_kraftstoff_bestand.getId());});
+		nav_kraftstoff_bestellungen.setOnAction(event -> {controller.onCall(nav_kraftstoff_bestellungen.getId());});
+		nav_kraftstoff_tanks.setOnAction(event -> {controller.onCall(nav_kraftstoff_tanks.getId());});
+		/**/
+		nav_personal_benutzer.setOnAction(event -> {controller.onCall(nav_personal_benutzer.getId());});
+		nav_personal_verwaltung.setOnAction(event -> {controller.onCall(nav_personal_verwaltung.getId());});
+		nav_personal_abmelden.setOnAction(event -> {controller.onCall(nav_personal_abmelden.getId());});
+		/**/
+		nav_fenster_dark.setOnAction(event -> {controller.onCall(nav_fenster_dark.getId());});
+		nav_fenster_light.setOnAction(event -> {controller.onCall(nav_fenster_light.getId());});
+		nav_fenster_doc.setOnAction(event -> {controller.onCall(nav_fenster_doc.getId());});
+		/**/
+		nav_user.setOnAction(event -> {controller.onCall(nav_user.getId());});
+	}
+	
+	void showUser(String vn, String nn) {
+		this.nav_user.setText(vn+" "+nn);
+		this.nav_bar.setDisable(false);
+	}
+	
+	void clearUser() {
+		this.nav_user.setText("");
+		this.nav_bar.setDisable(true);
+	}
+}
